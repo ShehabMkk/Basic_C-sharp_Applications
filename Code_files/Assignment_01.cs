b50c2a68b593c75d2cff0b67b38f3c40009e043c
Shehab Mkk <132353170+ShehabMkk@users.noreply.github.com>
2024-06-18 18:51:44 +0000

    1

---
+++ Code_files/Assignment_01.cs
@@ -0,0 +1,68 @@
+﻿//============================================================================================\\
+//===================================Assignment_01============================================\\
+//============================================================================================\\
+
+// 1- Write a program that allows the user to enter a 
+//    number then print it. 
+#region Question_1
+
+    Console.Write("Enter a number: ");
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out int num))
+    {
+        Console.WriteLine("You entered: " + num);
+    }
+    else Console.WriteLine("Invalid input. Please enter a valid number.");
+    
+#endregion
+
+//============================================================================================\\
+
+// 2- Write C# program that Assigning one value type 
+//    variable to another and modifying the value of one 
+//    variable and mention what will happen 
+#region Question_2
+
+    //  int num1 = 10;
+    //  int num2 = num1;
+    //
+    //  Console.WriteLine("num1: " + num1); // Outputs: 10
+    //  Console.WriteLine("num2: " + num2); // Outputs: 10
+    //
+    //  // Modifying the num2
+    //  num2 = 20;
+    //
+    //  Console.WriteLine("Modified num1: " + num1); // Outputs: 10
+    //  Console.WriteLine("Modified num2: " + num2); // Outputs: 20
+    //
+    // // What will happen:
+    // // num1 types store the actual data. When you assign one num1 type to another,
+    // // it creates a copy of the data. Modifying one does not affect the other.
+
+#endregion
+
+//============================================================================================\\
+
+// 3- Write C# program that Assigning one reference type 
+// variable to another and modifying the object through 
+// one variable and mention what will happen 
+#region Question_3
+
+    //  MyClass c1 = new MyClass(1,2);
+    //  MyClass c2 = c1;
+    //  Console.WriteLine("A from c1 =" + c1.A);
+    //  Console.WriteLine("A from c2 =" + c2.A);
+    //  c1.A++;
+    //  Console.WriteLine("A from c2 =" + c2.A);
+    //
+    //  class MyClass(int A, int B)
+    //  {
+    //      public int A=A;
+    //      public int B=B;
+    //  }
+    //
+    // // What will happen:
+    // // c1 opject store the actual data. When you assign one c1 opject to another,
+    // // it takes the data by refrense. Modifying one can affect the other.
+
+#endregion
