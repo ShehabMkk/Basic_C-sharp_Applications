a9c8762960b307a0fa9af5267bf2d020432af2de
Shehab Mkk <132353170+ShehabMkk@users.noreply.github.com>
2024-07-20 22:42:29 +0300

    Add files via upload

---
+++ Code_files/Program.cs
@@ -0,0 +1,48 @@
+﻿//============================================================================================\\
+//===================================Assignment_09============================================\\
+//============================================================================================\\
+
+namespace Assignment_09;
+using System;
+
+class Maths
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+        {
+            System.Console.WriteLine("Cannot divide by zero.");
+            return 0;
+        }
+        return a / b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        double num1 = 10;
+        double num2 = 5;
+
+        Console.WriteLine("Add: " + Maths.Add(num1, num2));
+        Console.WriteLine("Subtract: " + Maths.Subtract(num1, num2));
+        Console.WriteLine("Multiply: " + Maths.Multiply(num1, num2));
+        Console.WriteLine("Divide: " + Maths.Divide(num1, num2));
+    }
+}
